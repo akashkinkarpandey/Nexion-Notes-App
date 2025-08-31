4e93c8d1255c5c7eeda90a44cfb6733b70d695a0
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-08-31 20:13:58 +0530

    Deployment config changed

--- convex/auth.config.js
+++ convex/auth.config.js
@@ -1,7 +1,7 @@
 export default {
   providers: [
     {
-      domain: "https://solid-jaybird-63.clerk.accounts.dev",
+      domain: process.env.CLERK_JWT_ISSUER_DOMAIN || "",
       applicationID: "convex",
     },
   ],
